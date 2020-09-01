a0515cb6acc3d5f4b77207721b2d9529240e9797
marce <marcelo.leiteaj@gmail.com>
2020-09-01 17:38:26 -0300

    Correção na importação de dotenv

--- models/index.js
+++ models/index.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 import accountModel from './accountModel.js';
-require('dotenv').config();
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const db = {};
 
-db.url = `mongodb+srv://
-  ${process.env.USERDB}:${process.env.PWDDB}@bootcampigti.1dfvi.gcp.mongodb.net/banco?retryWrites=true&w=majority`;
+db.url = `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@bootcampigti.1dfvi.gcp.mongodb.net/banco?retryWrites=true&w=majority`;
 db.mongoose = mongoose;
 db.account = accountModel(mongoose);
 
